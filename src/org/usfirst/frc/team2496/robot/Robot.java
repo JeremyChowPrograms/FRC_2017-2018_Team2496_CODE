74f525e843bebe97d74affab444f11e412021210
Ashwin <ashwingupta2000@gmail.com>
2018-01-14 14:45:26 -0800

    added gyro support

--- src/org/usfirst/frc/team2496/robot/Robot.java
+++ src/org/usfirst/frc/team2496/robot/Robot.java
@@ -2,21 +2,24 @@ package org.usfirst.frc.team2496.robot;
 
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
+Gyro gyro;
 	
 
 	public Robot() {
-	
+		
 	}
 
 	@Override
 	public void robotInit() {
-	
+		gyro = new AnalogGyro(1);
 	}
 
 	
@@ -29,7 +32,7 @@ public class Robot extends SampleRobot {
 	public void operatorControl() { //Main thread
 		 
 		while (isOperatorControl() && isEnabled()) {
-			
+			SmartDashboard.putNumber("gyro: ", gyro.getAngle());
 			Timer.delay(0.005); 
 		}
 	}
